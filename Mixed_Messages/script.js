e423620f2d843df32fd1913bf3b269be952fe534
Higgy <higginsjake211@gmail.com>
2022-02-02 22:04:48 -0800

    add strings to arrays

--- Mixed_Messages/script.js
+++ Mixed_Messages/script.js
@@ -1,6 +1,6 @@
 //this program is intended to output the fortune of the day. Output will consist of three randomized sentences
 const fortune = {
-    situation: [],
-    seek: [],
-    avoid: []
-}
+    situation: ['you may be feeling down', 'you have good luck', 'the gods are pleased', 'the gods are angry'],
+    seek: ['reflect inward', 'go for a walk', 'sacrifice your firstborn child', 'read a book'],
+    avoid: ['being destroyed by the gods', 'the ocean', 'sitting in bed all day', 'overworking yourself']
+};
